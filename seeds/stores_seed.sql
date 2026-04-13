87a646ffb83f530a0cb31f3a3f2238c8ad897479
Ivan Yeung <ivanyeung0610@gmail.com>
2026-04-13 03:11:19 -0400

    Created a folder for seeding the database for testing.

---
+++ seeds/stores_seed.sql
@@ -0,0 +1,109 @@
+INSERT INTO stores (name, address, latitude, longitude, phone, website, hours, parking) VALUES
+(
+  'Associated Supermarket - Harlem',
+  '301 W 125th St, New York, NY 10027',
+  40.808100, -73.951400,
+  '(212) 222-5000',
+  NULL,
+  '{"mon":{"open":"07:00","close":"22:00"},"tue":{"open":"07:00","close":"22:00"},"wed":{"open":"07:00","close":"22:00"},"thu":{"open":"07:00","close":"22:00"},"fri":{"open":"07:00","close":"22:00"},"sat":{"open":"07:00","close":"22:00"},"sun":{"open":"08:00","close":"21:00"}}',
+  'Street'
+),
+(
+  'Whole Foods Market - Columbus Circle',
+  '10 Columbus Cir, New York, NY 10019',
+  40.768400, -73.983100,
+  '(212) 823-9600',
+  'https://www.wholefoodsmarket.com',
+  '{"mon":{"open":"07:00","close":"22:00"},"tue":{"open":"07:00","close":"22:00"},"wed":{"open":"07:00","close":"22:00"},"thu":{"open":"07:00","close":"22:00"},"fri":{"open":"07:00","close":"22:00"},"sat":{"open":"07:00","close":"22:00"},"sun":{"open":"07:00","close":"22:00"}}',
+  'Garage'
+),
+(
+  'Trader Joe''s - Union Square',
+  '142 E 14th St, New York, NY 10003',
+  40.734500, -73.989700,
+  '(212) 529-4612',
+  'https://www.traderjoes.com',
+  '{"mon":{"open":"08:00","close":"22:00"},"tue":{"open":"08:00","close":"22:00"},"wed":{"open":"08:00","close":"22:00"},"thu":{"open":"08:00","close":"22:00"},"fri":{"open":"08:00","close":"22:00"},"sat":{"open":"08:00","close":"22:00"},"sun":{"open":"08:00","close":"22:00"}}',
+  'None'
+),
+(
+  'Key Food - Astoria',
+  '24-60 Steinway St, Astoria, NY 11103',
+  40.772500, -73.929800,
+  '(718) 545-8800',
+  NULL,
+  '{"mon":{"open":"07:00","close":"21:00"},"tue":{"open":"07:00","close":"21:00"},"wed":{"open":"07:00","close":"21:00"},"thu":{"open":"07:00","close":"21:00"},"fri":{"open":"07:00","close":"21:00"},"sat":{"open":"07:00","close":"21:00"},"sun":{"open":"08:00","close":"20:00"}}',
+  'Lot'
+),
+(
+  'C-Town Supermarkets - Bushwick',
+  '1721 Myrtle Ave, Ridgewood, NY 11385',
+  40.700200, -73.905300,
+  '(718) 456-2100',
+  NULL,
+  '{"mon":{"open":"07:00","close":"21:00"},"tue":{"open":"07:00","close":"21:00"},"wed":{"open":"07:00","close":"21:00"},"thu":{"open":"07:00","close":"21:00"},"fri":{"open":"07:00","close":"21:00"},"sat":{"open":"07:00","close":"21:00"},"sun":{"open":"08:00","close":"20:00"}}',
+  'Street'
+),
+(
+  'Fairway Market - Red Hook',
+  '480-500 Van Brunt St, Brooklyn, NY 11231',
+  40.676800, -74.016200,
+  '(718) 694-6868',
+  'https://www.fairwaymarket.com',
+  '{"mon":{"open":"07:00","close":"23:00"},"tue":{"open":"07:00","close":"23:00"},"wed":{"open":"07:00","close":"23:00"},"thu":{"open":"07:00","close":"23:00"},"fri":{"open":"07:00","close":"23:00"},"sat":{"open":"07:00","close":"23:00"},"sun":{"open":"07:00","close":"23:00"}}',
+  'Lot'
+),
+(
+  'Stop & Shop - Bay Ridge',
+  '9315 5th Ave, Brooklyn, NY 11209',
+  40.625300, -74.029100,
+  '(718) 748-7200',
+  'https://www.stopandshop.com',
+  '{"mon":{"open":"07:00","close":"22:00"},"tue":{"open":"07:00","close":"22:00"},"wed":{"open":"07:00","close":"22:00"},"thu":{"open":"07:00","close":"22:00"},"fri":{"open":"07:00","close":"22:00"},"sat":{"open":"07:00","close":"22:00"},"sun":{"open":"07:00","close":"22:00"}}',
+  'Lot'
+),
+(
+  'Western Beef - Fordham',
+  '2840 Webster Ave, Bronx, NY 10458',
+  40.861600, -73.889400,
+  '(718) 220-5050',
+  NULL,
+  '{"mon":{"open":"07:00","close":"21:00"},"tue":{"open":"07:00","close":"21:00"},"wed":{"open":"07:00","close":"21:00"},"thu":{"open":"07:00","close":"21:00"},"fri":{"open":"07:00","close":"21:00"},"sat":{"open":"07:00","close":"21:00"},"sun":{"open":"08:00","close":"20:00"}}',
+  'Lot'
+),
+(
+  'Compare Foods - Washington Heights',
+  '795 W 181st St, New York, NY 10033',
+  40.849500, -73.938700,
+  '(212) 740-5800',
+  NULL,
+  '{"mon":{"open":"07:00","close":"21:00"},"tue":{"open":"07:00","close":"21:00"},"wed":{"open":"07:00","close":"21:00"},"thu":{"open":"07:00","close":"21:00"},"fri":{"open":"07:00","close":"21:00"},"sat":{"open":"07:00","close":"21:00"},"sun":{"open":"08:00","close":"20:00"}}',
+  'Street'
+),
+(
+  'Food Bazaar - Jackson Heights',
+  '7201 Roosevelt Ave, Jackson Heights, NY 11372',
+  40.746500, -73.891200,
+  '(718) 672-7100',
+  'https://www.foodbazaarsupermarket.com',
+  '{"mon":{"open":"07:00","close":"22:00"},"tue":{"open":"07:00","close":"22:00"},"wed":{"open":"07:00","close":"22:00"},"thu":{"open":"07:00","close":"22:00"},"fri":{"open":"07:00","close":"22:00"},"sat":{"open":"07:00","close":"22:00"},"sun":{"open":"07:00","close":"22:00"}}',
+  'Lot'
+),
+(
+  'Target - Staten Island',
+  '2795 Richmond Ave, Staten Island, NY 10314',
+  40.584500, -74.146800,
+  '(718) 477-6900',
+  'https://www.target.com',
+  '{"mon":{"open":"08:00","close":"22:00"},"tue":{"open":"08:00","close":"22:00"},"wed":{"open":"08:00","close":"22:00"},"thu":{"open":"08:00","close":"22:00"},"fri":{"open":"08:00","close":"22:00"},"sat":{"open":"08:00","close":"22:00"},"sun":{"open":"08:00","close":"21:00"}}',
+  'Lot'
+),
+(
+  'Associated Supermarket - Flushing',
+  '41-15 Main St, Flushing, NY 11355',
+  40.757300, -73.830500,
+  '(718) 353-6688',
+  NULL,
+  '{"mon":{"open":"07:00","close":"21:00"},"tue":{"open":"07:00","close":"21:00"},"wed":{"open":"07:00","close":"21:00"},"thu":{"open":"07:00","close":"21:00"},"fri":{"open":"07:00","close":"21:00"},"sat":{"open":"07:00","close":"21:00"},"sun":{"open":"08:00","close":"20:00"}}',
+  'Street'
+);
