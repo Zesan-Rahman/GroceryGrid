9b80b54a2a45af034656fd61853486454f6b6ed2
Jing Qian <jqian2272@gmail.com>
2026-04-21 23:21:21 -0400

    chore: add a dummy account that you can sign in as

--- seeds/accounts_seed.sql
+++ seeds/accounts_seed.sql
@@ -1,3 +1,9 @@
 --note you will never be able to log in as these accounts
 INSERT INTO accounts (email, password, name, role) VALUES
 ('account1@example.com', 'hashed_password?', 'Example User', 'user');
+
+--you can (probably) log in as this account
+--account: a@a.com
+--password: password
+INSERT INTO accounts (email, password, name, role) VALUES
+('a@a.com', '$2b$12$dZM7ZECvocN5.RR7HjlPp.BGACcc2Rlh81EIVBNdg7It6sNMRSZOe', 'a user', 'user');
