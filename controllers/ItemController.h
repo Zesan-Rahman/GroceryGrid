dd9dbac950a8fe757ea3082ed337b83b29f673db
Jing Qian <jqian2272@gmail.com>
2026-04-26 05:03:28 -0400

    feat: endpoint to fetch price history of an item

--- controllers/ItemController.h
+++ controllers/ItemController.h
@@ -8,6 +8,7 @@ public:
   METHOD_LIST_BEGIN
   ADD_METHOD_TO(ItemController::listItems, "/api/items", drogon::Get);
   ADD_METHOD_TO(ItemController::getItem, "/api/items/{1}", drogon::Get);
+  ADD_METHOD_TO(ItemController::getPriceHistory, "/api/items/{1}/price-history", drogon::Get);
   METHOD_LIST_END
 
   void listItems(
@@ -17,5 +18,10 @@ public:
   void getItem(const drogon::HttpRequestPtr &req,
                std::function<void(const drogon::HttpResponsePtr &)> &&callback,
                int itemId) const;
+
+  void getPriceHistory(
+      const drogon::HttpRequestPtr &req,
+      std::function<void(const drogon::HttpResponsePtr &)> &&callback,
+      int itemId) const;
 };
 } // namespace api
